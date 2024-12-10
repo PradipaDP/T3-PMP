9a3fdfe36816e8cad688bd7eb350e30d161d928a
PradipaDP <kray210104@gmail.com>
2024-12-11 00:27:54 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Route untuk halaman home
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+# Route untuk halaman tentang saya
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+# Route untuk halaman hobi
+@app.route('/hobbies')
+def hobbies():
+    return render_template('hobbies.html')
+
+# Route untuk halaman project
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
